6647fd4c77d379c44d70560cb3fb47a7053668fb
Andrew Lobov <lobovav@live.com>
2020-04-11 16:23:29 +0300

    Add simple click counter

---
+++ components/ClickCounter.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="click-counter">
+    <button type="button" @click="clickHandler">Click counts {{ clickCounts }}</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'click-counter',
+/*   action: function() {
+
+  } */
+  data: function() {
+    return {
+      clickCounts: 0,
+      clickEvents: [],
+      user: {
+        name: 'Andrew',
+        lastName: 'Lobov'
+      }
+    }
+  },
+
+  methods: {
+    clickHandler(){
+      this.clickCounts += 1;
+      console.log(this.clickCounts)
+    }
+  }
+}
+
+</script>
+<style lang="css" scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -25,17 +25,19 @@
         </a>
         <nuxt-link :to="{name: 'virus'}">COVID-19 stats</nuxt-link>
       </div>
-      <el-button>click</el-button>
+      <ClickCounter></ClickCounter>
+      <click-counter></click-counter>
     </div>
   </div>
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
-
+import ClickCounter from '~/components/ClickCounter'
 export default {
   components: {
-    Logo
+    Logo,
+    ClickCounter
   }
 }
 </script>
